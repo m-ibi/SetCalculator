8c108c4f398d44ed4cb602cfec2c30afc668eb52
m-ibi <46395585+m-ibi@users.noreply.github.com>
2024-10-20 21:35:46 +0500

    Add files via upload
    basic set functions in python

---
+++ functions.py
@@ -0,0 +1,18 @@
+# functions.py
+
+def union(set1, set2):
+
+    return set1 | set2
+
+def intersection(set1, set2):
+
+    return set1 & set2
+
+def difference(set1, set2):
+
+    return set1 - set2
+
+def symmetric_difference(set1, set2):
+
+    return set1 ^ set2
+
